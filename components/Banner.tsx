9aead4124bf99f1af41d853263861bd6bea573f8
Morhaf Ghziel <ghzielmorhaf@gmail.com>
2026-09-02 22:06:07 +0300

    chore: remove legacy root components dir

--- components/Banner.tsx
+++
@@ -1,8 +0,0 @@
-const Banner = () => {
-  return (
-    <div className="w-full bg-black py-8">
-    </div>
-  );
-};
-
-export default Banner;
--- components/TopBanner.tsx
+++
@@ -1,8 +0,0 @@
-const TopBanner = () => {
-  return (
-    <div className="w-full bg-black py-4">
-    </div>
-  );
-};
-
-export default TopBanner;
